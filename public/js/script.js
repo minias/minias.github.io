cfc3a7e70f671d98d042a456804e495dcfc5235f
minias <gnuhacker@gmail.com>
2024-05-10 10:41:04 +0800

    gotop js fix

--- public/js/script.js
+++ public/js/script.js
@@ -21,22 +21,25 @@ for (var i = 0, length = links.length; i < length; i++) {
   }
 }
 
-var backToTop = () => {
-  // Scroll | button show/hide
-  window.addEventListener('scroll', () => {
-    if (document.querySelector('html').scrollTop > 100) {
-      document.getElementById('go-top').style.display = "block";
-    } else {
-      document.getElementById('go-top').style.display = "none";
-    }
-  });
-  // back to top
-  document.getElementById('go-top').addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
+window.addEventListener("DOMContentLoaded", (event) => {
+  //const el = document.getElementById('scroll');
+  if (document.getElementById('scroll')) {
+    // Scroll | button show/hide
+    window.addEventListener('scroll', () => {
+      if (document.querySelector('html').scrollTop > 100) {
+        document.getElementById('go-top').style.display = "block";
+      } else {
+        document.getElementById('go-top').style.display = "none";
+      }
     });
-  })
-};
-backToTop(); 
+    // back to top
+    document.getElementById('go-top').addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    })
+  }
+});
+ 
